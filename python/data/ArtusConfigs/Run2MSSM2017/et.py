a6f371d827ded0b465b10f32c9077a95875895ac
Olena Hlushchenko <greyxray@gmail.com>
2018-12-14 16:26:35 +0100

    cleanup

--- python/data/ArtusConfigs/Run2MSSM2017/et.py
+++ python/data/ArtusConfigs/Run2MSSM2017/et.py
@@ -443,19 +443,13 @@ def build_config(nickname, **kwargs):
 
   # pipelines - systematic shifts
   if etau_fake_es and pipelines is not None:
-    # nominal tauESperDM_shifts et_eleFakeTauES_subanalysis METunc_shifts METrecoil_shifts JECunc_shifts
+    # needed : nominal, tauESperDM_shifts, et_eleFakeTauES_subanalysis, maybe METunc_shifts METrecoil_shifts JECunc_shifts
     return_conf = jsonTools.JsonDict()
     for pipeline in pipelines:
       print 'add pipe:', pipeline
       return_conf += ACU.apply_uncertainty_shift_configs('et', config, importlib.import_module("HiggsAnalysis.KITHiggsToTauTau.data.ArtusConfigs.Run2MSSM2017." + pipeline).build_config(nickname, **kwargs))
+
     return return_conf
-
-    # nominal, TES
-    return ACU.apply_uncertainty_shift_configs('et', config, importlib.import_module("HiggsAnalysis.KITHiggsToTauTau.data.ArtusConfigs.Run2MSSM2017.nominal").build_config(nickname)) \
-         + ACU.apply_uncertainty_shift_configs('et', config, importlib.import_module("HiggsAnalysis.KITHiggsToTauTau.data.ArtusConfigs.Run2MSSM2017.tauESperDM_shifts").build_config(nickname))
-
-    # FES
-    # return ACU.apply_uncertainty_shift_configs('et', config, importlib.import_module("HiggsAnalysis.KITHiggsToTauTau.data.ArtusConfigs.Run2MSSM2017.et_eleFakeTauES_subanalysis").build_config(nickname, **kwargs))
 
   return ACU.apply_uncertainty_shift_configs('et', config, importlib.import_module("HiggsAnalysis.KITHiggsToTauTau.data.ArtusConfigs.Run2MSSM2017.nominal").build_config(nickname)) + \
          ACU.apply_uncertainty_shift_configs('et', config, importlib.import_module("HiggsAnalysis.KITHiggsToTauTau.data.ArtusConfigs.Run2MSSM2017.tauESperDM_shifts").build_config(nickname)) + \
