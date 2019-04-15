f7bf60bf319632112d0303a612ddf0dcf55466d3
Olena Hlushchenko <greyxray@gmail.com>
2019-04-15 16:22:50 +0200

    checkout:use ssh

--- scripts/checkout_packages_CMSSW102X.sh
+++ scripts/checkout_packages_CMSSW102X.sh
@@ -24,7 +24,7 @@ git cms-addpkg CondFormats/JetMETObjects
 
 # From Kappa, only the DataFormats are needed
 # Mind that for certain skims, you need exactly the Kappa git tag that has been used for the production
-git clone https://github.com/KIT-CMS/Kappa.git -b dictchanges_10_2
+git clone git@github.com:KIT-CMS/Kappa.git -b dictchanges_10_2
 pushd Kappa
 echo docs/ >> .git/info/sparse-checkout
 echo DataFormats/ >> .git/info/sparse-checkout
@@ -33,36 +33,36 @@ git config core.sparsecheckout true
 git read-tree -mu HEAD
 popd
 
-git clone https://github.com/KIT-CMS/KappaTools.git -b master
+git clone git@github.com:KIT-CMS/KappaTools.git -b master
 
-git clone https://github.com/KIT-CMS/Artus.git -b reduced_trigger_objects
+git clone git@github.com:KIT-CMS/Artus.git -b reduced_trigger_objects
 
 # checkout KITHiggsToTauTau CMSSW analysis package
-git clone https://github.com/KIT-CMS/KITHiggsToTauTau HiggsAnalysis/KITHiggsToTauTau -b reduced_trigger_objects
+git clone git@github.com:KIT-CMS/KITHiggsToTauTau HiggsAnalysis/KITHiggsToTauTau -b reduced_trigger_objects
 
 # quantile mapping package
-git clone https://github.com/KIT-CMS/quantile_mapping
+git clone git@github.com:KIT-CMS/quantile_mapping
 
 # Svfit
-git clone https://github.com/svfit/ClassicSVfit TauAnalysis/ClassicSVfit
+git clone git@github.com:svfit/ClassicSVfit TauAnalysis/ClassicSVfit
 cd TauAnalysis/ClassicSVfit
 git checkout c78af4dc0f54cdc1c0d4b4fc4879918cfa2527c9
 cd -
-git clone https://github.com/svfit/SVfitTF TauAnalysis/SVfitTF
+git clone git@github.com:svfit/SVfitTF TauAnalysis/SVfitTF
 
 # Jet2Tau Fakes
-git clone https://github.com/CMS-HTT/Jet2TauFakes.git HTTutilities/Jet2TauFakes
+git clone git@github.com:CMS-HTT/Jet2TauFakes.git HTTutilities/Jet2TauFakes
 
 # EmuQCD Method
-git clone https://github.com/CMS-HTT/QCDModelingEMu.git HTT-utilities/QCDModelingEMu
+git clone git@github.com:CMS-HTT/QCDModelingEMu.git HTT-utilities/QCDModelingEMu
 
 sed '/CombineHarvester/d' ${CMSSW_BASE}/src/HiggsAnalysis/KITHiggsToTauTau/BuildFile.xml -i
 
 # TauTriggerSFs2017 tool
-git clone https://github.com/truggles/TauTriggerSFs2017.git  TauAnalysisTools/TauTriggerSFs -b final_2017_MCv2
+git clone git@github.com:truggles/TauTriggerSFs2017.git  TauAnalysisTools/TauTriggerSFs -b final_2017_MCv2  # for 2017 triggers
 
 # Grid-Control
-git clone https://github.com/janekbechtel/grid-control.git
+git clone git@github.com:janekbechtel/grid-control.git
 
 # source ini script, needs to be done in every new shell
 source HiggsAnalysis/KITHiggsToTauTau/scripts/ini_KITHiggsToTauTauAnalysis.sh
