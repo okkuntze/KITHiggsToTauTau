848ac02980f56af95ae6f8627b334a8de3b41093
Artur Gottmann <artur.gottmann@kit.edu>
2019-05-08 20:12:05 +0200

    Run2LegacyAnalysis, settingsJetID.py: extend configuration for 2017 ee noise jets (currently still disabled)

--- python/data/ArtusConfigs/Run2LegacyAnalysis/Includes/settingsJetID.py
+++ python/data/ArtusConfigs/Run2LegacyAnalysis/Includes/settingsJetID.py
@@ -15,7 +15,7 @@ import os
 
 def build_config(nickname, **kwargs):
 
-  #TODO: 1) update Jet ID's as recommended for year 2018, 2) implement pileup JetID to remove ee noise in 2017 as alternative approach
+  #TODO: 1) update Jet ID's as recommended for year 2018
   config = jsonTools.JsonDict()
   datasetsHelper = datasetsHelperTwopz.datasetsHelperTwopz(os.path.expandvars("$CMSSW_BASE/src/Kappa/Skimming/data/datasets.json"))
   year = datasetsHelper.base_dict[nickname]["year"]
@@ -41,6 +41,10 @@ def build_config(nickname, **kwargs):
   config["JetUpperAbsEtaCuts"] = ["4.7"]
   config["JetLeptonLowerDeltaRCut"] = 0.5 
   if year in [2016,2018]: config["JetApplyEENoiseVeto"] = False
-  elif year == 2017:      config["JetApplyEENoiseVeto"] = False #TODO activate for 2017 on new skim; allow PU JetID as alternative
+  elif year == 2017:
+    config["JetApplyEENoiseVeto"] = False #TODO: enable as soon as uncorrected P4 is available in skim
+    config["JetApplyPUIDForEENoise"] = True
+    config["JetPUIDForEENoiseName"] = "pileupJetIdUpdatedfullId"
+    config["JetPUIDForEENoiseWP"] = "loose"
 
   return config
