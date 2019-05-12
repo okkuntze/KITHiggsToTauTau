f2b46cd0250c1e0f8904703cfe84ff5320539b8b
Artur Gottmann <artur.gottmann@kit.edu>
2019-05-12 22:45:27 +0200

    Run2LegacyAnalysis: add muonES shifts also to em channel

--- python/data/ArtusConfigs/Run2LegacyAnalysis/2017/em.py
+++ python/data/ArtusConfigs/Run2LegacyAnalysis/2017/em.py
@@ -462,7 +462,7 @@ def build_config(nickname, **kwargs):
      config["Consumers"].append("BTagEffConsumer")
 
   # pipelines - systematic shifts
-  needed_pipelines = ['nominal', 'eleES_shifts', 'regionalJECunc_shifts', 'METunc_shifts', 'METrecoil_shifts', 'btagging_shifts']
+  needed_pipelines = ['nominal', 'eleES_shifts', 'regionalJECunc_shifts', 'METunc_shifts', 'METrecoil_shifts', 'btagging_shifts', 'muonES_shifts']
   if pipelines is None:
       raise Exception("pipelines is None in %s" % (__file__))
   elif 'auto' in pipelines:
